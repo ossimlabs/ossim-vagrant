1f5a41f8135e74207c11dfb9dafeb97cf219de5f
Garrett Potts <gpotts@radiantblue.com>
2017-03-20 09:47:25 -0400

    Made the ENV point to ToolKit

--- salt/dev-build/build-env.sh
+++ salt/dev-build/build-env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export OSSIM_DEV_HOME=~
 export JAVA_HOME=/usr/lib/jvm/java
-export MSP_HOME=/msp
-export MSP_DATA_DIR=$MSP_HOME/ToolKit/data
-export CSM_PLUGIN_DIR=$MSP_HOME/ToolKit/plugins
+export MSP_HOME=/msp/ToolKit
+export MSP_DATA_DIR=$MSP_HOME/data
+export CSM_PLUGIN_DIR=$MSP_HOME/plugins
 export OSSIM_DEPENDENCIES=$OSSIM_DEV_HOME/ossim-dependencies
 export OSSIM_INSTALL_PREFIX=$OSSIM_DEV_HOME/ossim-install
 export OSSIM_BUILD_DIR=$OSSIM_DEV_HOME/build
